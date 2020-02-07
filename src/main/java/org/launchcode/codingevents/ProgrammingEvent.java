83e718c0b8edea53b15966f1cc75aac2e4ab050d
James Ginns <starvagrant@yahoo.com>
2020-02-07 17:43:45 -0600

    Use a List of Custom Programming Event Objects to hold code.
    The exercise wants you to use a HashMap for something a HashMap
    isn't supposed to be used for (a list where all values are
    meaningful and need to be accessed like a list. So create a
    ProgrammingEvent object and store data in that. This this
    also considerably easier at extension than a HashMap is.

---
+++ src/main/java/org/launchcode/codingevents/ProgrammingEvent.java
@@ -0,0 +1,11 @@
+package org.launchcode.codingevents;
+
+public class ProgrammingEvent {
+    public String name;
+    public String description;
+
+    public ProgrammingEvent(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,24 +1,32 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.ProgrammingEvent;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
     @GetMapping
-    public String getEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Kansas Linux Fest");
-        events.add("Java Users Group");
-        events.add("Strange Loop");
-        model.addAttribute("events", events);
+    public String displayAllEvents(Model model) {
+        Map<String, String> events = new HashMap<>();
+        events.put("Kansas Linux Fest", "Annual Kansas Linux Conference");
+        events.put("Java Users Group", "Monthly Java Meetup");
+        events.put("Strange Loop", "Yearly St. Louis Tech Conference");
+        List<ProgrammingEvent> eventList = new ArrayList<>();
+        for(String key : events.keySet()) {
+            eventList.add(new ProgrammingEvent(key, events.get(key)));
+        }
+        model.addAttribute("events", eventList);
+
         return "events/index";
     }
 }
